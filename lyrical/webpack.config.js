2bd01af2f0bec911f86f4e77947679acf5a69d44
Joseph C <jccollett83@gmail.com>
2020-04-12 18:03:24 -0700

    Middle of Section 6 Clientside GraphQL

---
+++ lyrical/webpack.config.js
@@ -0,0 +1,58 @@
+// const webpack = require('webpack');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   entry: './client/index.js',
+//   output: {
+//     path: '/',
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         use: 'babel-loader',
+//         test: /\.js$/,
+//         exclude: /node_modules/
+//       },
+//       {
+//         use: ['style-loader', 'css-loader'],
+//         test: /\.css$/
+//       }
+//     ]
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: 'client/index.html'
+//     })
+//   ]
+// };
+
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.js',
+  output: {
+    path: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        use: { loader: 'babel-loader' },
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'client/index.html',
+    }),
+  ],
+};
